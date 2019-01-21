1fe8da309b906fd4904d910c107d8643161c19b8
lisa copeland <hello@stopdaydreaming.com>
2019-01-21 17:00:07 -0500

    removed merge markers

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
 import React, {Component} from 'react';
 
 class QuizEnd extends Component {
   render(){
     return(
-=======
-import React, {Component} from 'react'
-
-class QuizEnd extends Component {
-  render() {
-    return (
->>>>>>> 1a94bacbcc337a29421cca8f33cc167539426bdf
       <div>
         <p>Thanks for playing!</p>
         <a href=''>Reset Quiz</a>
