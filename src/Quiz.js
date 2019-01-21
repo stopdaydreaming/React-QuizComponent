9aa606cb30fa77b4fd3d6d4ea8e7ba18bb8b8320
lisa copeland <hello@stopdaydreaming.com>
2019-01-21 16:55:13 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,6 +1,7 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
 
-import QuizQuestion from './QuizQuestion.js'
+import QuizQuestion from './QuizQuestion';
+import QuizEnd from './QuizEnd';
 
 let quizData = require('./quiz_data.json')
 
@@ -9,9 +10,16 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
-  render(){
+  render() {
+    const isQuizEnd = ((this.state.quiz_position-1) ===
+    quizData.quiz_questions.length);
+
     return (
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} />
+      <div>
+        {isQuizEnd ? <QuizEnd /> :
+        <QuizQuestion quiz_question={quizData.quiz_questions
+        [this.state.quiz_position - 1]} /> }
+      </div>
     )
   }
 }
