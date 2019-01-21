55d96e6ddf96d8838c5327f8118d1591129c1bbb
lisa copeland <hello@stopdaydreaming.com>
2019-01-21 17:33:56 -0500

    Completed the module

--- src/Quiz.js
+++ src/Quiz.js
@@ -10,6 +10,11 @@ class Quiz extends Component {
     super(props)
     this.state = { quiz_position: 1 }
   }
+  showNextQuestion(){
+    this.setState((state) => {
+      return { quiz_position: state.quiz_position + 1}
+    })
+  }
   render() {
     const isQuizEnd = ((this.state.quiz_position-1) ===
     quizData.quiz_questions.length);
@@ -17,8 +22,7 @@ class Quiz extends Component {
     return (
       <div>
         {isQuizEnd ? <QuizEnd /> :
-        <QuizQuestion quiz_question={quizData.quiz_questions
-        [this.state.quiz_position - 1]} /> }
+        <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} showNextQuestionHandler={this.showNextQuestion.bind(this)}/> }
       </div>
     )
   }
